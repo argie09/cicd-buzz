0f5f01c1b5ed3a2030c2f110178c718fb035db8e
Ari <argie09@gmail.com>
2018-05-30 21:09:47 +0300

    Step 10 style b

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-	# page = '<html><body style="background-color:black;"><h1 style="color:white;">'
-    page = '<html><body><h1>'
+	page = '<html><body style="background-color:black;"><h1 style="color:white;">'
+    #page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
