6a523994c0731bc79a7949a51269f557d63f5b90
Ari <argie09@gmail.com>
2018-05-30 21:39:33 +0300

    Step 10 unstyled

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-	page = '<html><body'
-	page += '><h1>'
+	page = '<html><body><h1>'
 	page += generator.generate_buzz()
 	page += '</h1></body></html>'
 	return page
